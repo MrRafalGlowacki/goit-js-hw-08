5f943665685f451472394847127f5cc6d3ad81c7
Rafał Głowacki <107753644+MrRafalGlowacki@users.noreply.github.com>
2022-11-11 15:54:53 +0100

    first HW

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css"
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const div = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const img = galleryItems
+  .map(
+    (image) =>
+      `
+    <a class="gallery__item" href=${image.original}>
+      <img
+        class="gallery__image"
+        src=${image.preview}
+        
+        alt="${image.description}"
+      />
+    </a>
+  `
+  )
+  .join("");
+div.insertAdjacentHTML("beforeend", img);
+
+const lightbox = new SimpleLightbox(`.gallery a`, {
+  disableRightClick: true,
+  captionsData: "alt",
+  captionDelay: 250,
+});
