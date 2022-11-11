d9c5da0d3b24c57a405dbecb731368bd9867e4a4
Rafał Głowacki <107753644+MrRafalGlowacki@users.noreply.github.com>
2022-11-11 17:11:23 +0100

    hw 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe#vimeo-player');
+const player = new Player(iframe);
+
+const onPlay = ({ duration, percent, seconds }) => {
+  console.log(seconds);
+  localStorage.setItem('videoplayer-current-time', seconds);
+};
+
+player.on('timeupdate', _.throttle(onPlay, 1000));
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+const playStart = () => {
+  if (!savedTime) return 0;
+  return savedTime;
+};
+
+player.setCurrentTime(playStart());
