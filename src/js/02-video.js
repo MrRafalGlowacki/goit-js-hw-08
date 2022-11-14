7e7efc966bd420d06a50391a80be715d60a2bbe0
Rafał Głowacki <107753644+MrRafalGlowacki@users.noreply.github.com>
2022-11-14 17:14:31 +0100

    homework done 1.3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,18 +2,17 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe#vimeo-player');
 const player = new Player(iframe);
+const savedTime = Number(localStorage.getItem('videoplayer-current-time'));
 
 const onPlay = ({ duration, percent, seconds }) => {
-  console.log(seconds);
   localStorage.setItem('videoplayer-current-time', seconds);
 };
 
-player.on('timeupdate', throttle(onPlay, 1000));
-
-const savedTime = localStorage.getItem('videoplayer-current-time');
-const playStart = () => {
-  if (!savedTime) return 0;
-  return savedTime;
+const removeLocSto = () => {
+  if (savedTime > 568) {
+    localStorage.removeItem('videoplayer-current-time');
+  }
 };
-
-player.setCurrentTime(playStart());
+player.on('timeupdate', throttle(onPlay, 1000));
+player.setCurrentTime(savedTime);
+window.addEventListener('load', removeLocSto);
