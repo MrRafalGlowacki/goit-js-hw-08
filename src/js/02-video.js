8042ec281eb483e24b81ee8188d40d0d6d0c44ac
Rafał Głowacki <107753644+MrRafalGlowacki@users.noreply.github.com>
2022-11-14 17:34:17 +0100

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,16 +3,15 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe#vimeo-player');
 const player = new Player(iframe);
 const savedTime = Number(localStorage.getItem('videoplayer-current-time'));
-
+const startTimeHandler = () => {
+  if (savedTime > 568) {
+    localStorage.removeItem('videoplayer-current-time');
+  } else if (!savedTime) return 0;
+  else return savedTime;
+};
 const onPlay = ({ duration, percent, seconds }) => {
   localStorage.setItem('videoplayer-current-time', seconds);
 };
 
-const removeLocSto = () => {
-  if (savedTime > 568) {
-    localStorage.removeItem('videoplayer-current-time');
-  }
-};
 player.on('timeupdate', throttle(onPlay, 1000));
-player.setCurrentTime(savedTime);
-window.addEventListener('load', removeLocSto);
+player.setCurrentTime(startTimeHandler());
